abf79c55038738006e16548bba8539d3026c4152
suryaelidanto <suryaelidanto@gmail.com>
2024-10-04 10:51:09 +0700

    chore(.env.sample): update env sample

--- src/middlewares/redis.ts
+++ src/middlewares/redis.ts
@@ -1,36 +1,35 @@
-import { NextFunction, Request, Response } from 'express'
-import { VibeType, VibeWithDetailType } from '../types/types'
-import { redisClient } from '../libs/redis'
+import { NextFunction, Request, Response } from "express";
+import { VibeType, VibeWithDetailType } from "../types/types";
+import { redisClient } from "../libs/redis";
 
-import ResponseDTO from '../dtos/ResponseDTO'
+import ResponseDTO from "../dtos/ResponseDTO";
 
 class Redis {
-    async getVibes(req: Request, res: Response, next: NextFunction) {
-        const rawVibes = await redisClient.get('VIBES')
-        const vibes = JSON.parse(rawVibes)
+  async getVibes(req: Request, res: Response, next: NextFunction) {
+    const vibes = await redisClient.get("VIBES");
 
-        if (vibes) {
-            return res.status(200).json(
-                new ResponseDTO<VibeWithDetailType[]>({
-                    error: false,
-                    message: {
-                        status: 'Vibes retrieved!',
-                    },
-                    data: vibes,
-                })
-            )
-        }
-
-        next()
+    if (vibes) {
+      return res.status(200).json(
+        new ResponseDTO<VibeWithDetailType[]>({
+          error: false,
+          message: {
+            status: "Vibes retrieved!",
+          },
+          data: vibes,
+        })
+      );
     }
 
-    async setVibes(vibes: VibeType[]) {
-        await redisClient.set('VIBES', JSON.stringify(vibes))
-    }
+    next();
+  }
 
-    async deleteVibes() {
-        await redisClient.del('VIBES')
-    }
+  async setVibes(vibes: VibeType[]) {
+    await redisClient.set("VIBES", vibes);
+  }
+
+  async deleteVibes() {
+    await redisClient.del("VIBES");
+  }
 }
 
-export default new Redis()
+export default new Redis();
